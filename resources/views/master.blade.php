db0c5c6cf750ca6643d72c3d106aec7ed656cdf1
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2020-06-04 19:26:21 +0700

    init

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -256,11 +256,11 @@
 
   @yield('konten')
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+  <strong>Copyright &copy; 2020 <a href="{{url('/')}}">Mahsus Afandi</a>.</strong>
     All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+    {{-- <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.0.4
-    </div>
+    </div> --}}
   </footer>
 
   <!-- Control Sidebar -->
