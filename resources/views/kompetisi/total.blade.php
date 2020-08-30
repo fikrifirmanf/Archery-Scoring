745afae9b2e6683ca469b6c8ef7b014aced2809a
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2020-08-30 21:29:52 +0700

    fix

--- resources/views/kompetisi/total.blade.php
+++ resources/views/kompetisi/total.blade.php
@@ -24,7 +24,7 @@
       <div class="row">
         <div class="col-12"> 
         <div class="card">
-          
+          {{$totalall}}
             <div class="card-header">
               
               </div>
